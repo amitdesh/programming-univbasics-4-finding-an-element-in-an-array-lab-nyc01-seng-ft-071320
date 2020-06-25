9008aa0c7f8221ba96352830f8152e306e10f860
A D <amitdesh2014@gmail.com>
2020-06-25 01:00:36 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,3 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  while array[counter] do
-    if array[counter] == value_to_find
-      right_index = counter
-      return array.index
-      end
-    counter +=1
-  end
+  array.index(value_to_find)
 end
